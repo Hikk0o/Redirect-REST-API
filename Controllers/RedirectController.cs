ce010e4dda112bf6cbd56f3af45453da2ee70ef1
Ivan <78872275+Hikk0o@users.noreply.github.com>
2022-06-02 21:20:17 +0300

    Commit

---
+++ Controllers/RedirectController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RedirectAPI.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class RedirectController : ControllerBase
+{
+    [HttpGet(Name = "GetUrlFromShort")]
+    public string Get(string r)
+    {
+        return RedirectAPI.Redirect.GetUrl(r);
+    }
+    
+    [HttpPost(Name = "SetUrlFromShort")]
+    public string Post(string r)
+    {
+        return RedirectAPI.Redirect.SetNewUrl(r);
+    }
+}
---
+++ Redirect.cs
@@ -0,0 +1,73 @@
+﻿using Newtonsoft.Json;
+
+namespace RedirectAPI;
+
+public class Redirect
+{
+    public static bool IsDev { get; set; }
+    private const string FileName = "config.json";
+    private static readonly string PathToDataJson = IsDev? $"A:\\RiderProjects\\RedirectAPI\\RedirectAPI\\{FileName}" : $"./{FileName}";
+
+    private static async Task<Dictionary<string,object>?> LoadShortsJson()
+    {
+        if (!File.Exists(PathToDataJson))
+        {
+            await Task.Run(() =>
+            {
+                File.WriteAllText(PathToDataJson, "{\n}");
+            });
+        }
+
+        var json = await File.ReadAllTextAsync(PathToDataJson);
+        var responseJson = JsonConvert.DeserializeObject<Dictionary<string, object>>(json);
+        return responseJson;
+        
+    }
+    private static void UpdateShortsJson(Dictionary<string, object> dict)
+    {
+        var entries = dict.Select(d =>
+            $"\"{d.Key}\": \"{string.Join(",", d.Value)}\"");
+        
+        string newData = "{\n\t" + string.Join(",\n\t", entries) + "\n}";
+        File.WriteAllText(PathToDataJson, newData);
+        // Console.WriteLine("UpdateDataJson");
+    }
+
+    public static string GetUrl(string shortUrl)
+    {
+        var dictionary = LoadShortsJson().Result;
+        if (dictionary == null) return "null";
+        try
+        {
+            var url = (string)dictionary[shortUrl];
+            return url;
+        }
+        catch (KeyNotFoundException e)
+        {
+            return "KeyNotFound";
+        }
+    }
+
+    public static string SetNewUrl(string longUrl)
+    {
+        string numbers = "1234567890";
+        string letters = "abcdefghijklmnopqrstuvwxyz";
+        var chars = new char[8];
+        var random = new Random();
+        var finalChars = $"{letters}{letters.ToUpper()}{numbers}";
+        
+        for (int i = 0; i < chars.Length; i++)
+        {
+            chars[i] = finalChars[random.Next(finalChars.Length)];
+        }
+
+        if (GetUrl(new string(chars)) != "KeyNotFound") return SetNewUrl(longUrl);
+        var shorts = LoadShortsJson().Result;
+        if (shorts == null) return "ServerError";
+        shorts[new string(chars)] = longUrl;
+        UpdateShortsJson(shorts);
+
+        return new string(chars);
+    }
+
+}
